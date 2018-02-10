c9999856289d08ec26a551ddaf6545cc0aa012e9
Maria Catalina Camacho <m.catalina.camacho@gmail.com>
2018-02-10 15:16:10 -0800

    make template script for the VM

--- CorticalThicknessProcessing.py
+++ CorticalThicknessProcessing.py
@@ -11,22 +11,26 @@ from nipype.interfaces.io import SelectFiles, DataSink, FreeSurferSource
 from nipype.interfaces.fsl.preprocess import FAST
 from nipype.interfaces.fsl.utils import Reorient2Std
 from nipype.interfaces.freesurfer import FSCommand, MRIConvert
+from os import listdir
 
 # Set up study specific variables
 #project_home = '/Volumes/iang/active/ELS/ELS_FreeSurfer/Analysis'
 #project_home = '/Users/myelin/Dropbox/data/fs_practice'
-project_home = '/Users/catcamacho/Dropbox/Projects/AC_ELS_Cortex/proc/fs_practice'
+#project_home = '/Users/catcamacho/Dropbox/Projects/AC_ELS_Cortex/proc/fs_practice'
+project_home = '/share/iang/active/ELS/ELS_FreeSurfer/Analysis'
 
 #fs_subjdir = '/Volumes/iang/active/ELS/ELS_FreeSurfer/ELS_FS_subjDir'
 #fs_subjdir = '/Users/myelin/Dropbox/data/fs_practice/ELS_FS_subjDir'
-fs_subjdir = '/Users/catcamacho/Dropbox/Projects/AC_ELS_Cortex/proc/fs_practice/ELS_FS_subjDir'
+#fs_subjdir = '/Users/catcamacho/Dropbox/Projects/AC_ELS_Cortex/proc/fs_practice/ELS_FS_subjDir'
+fs_subjdir = '/share/iang/active/ELS/ELS_FreeSurfer/Analysis/proc/template_fs6' 
 
 workflow_dir = project_home + '/workflows'
 subj_proc = project_home + '/proc/subject'
 group_proc = project_home + '/proc/group'
 template_proc = project_home + '/proc/template'
-subject_info = project_home + '/misc/subjects.csv' 
-template_sub = ['011-T1']
+#subject_info = project_home + '/misc/subjects.csv' 
+#template_sub = ['011-T1']
+template_sub=listdir(fs_subjdir)
 
 #set default FreeSurfer subjects dir
 FSCommand.set_default_subjects_dir(fs_subjdir)
@@ -117,7 +121,7 @@ makeTemplate.inputs.output_prefix='ELS_CT_'
 
 
 ######### Template creation workflow #########
-template_flow = Workflow(name = "template_flow")
+template_flow = Workflow(name = 'template_flow')
 template_flow.connect([(fs_source, convertT1, [('T1','in_file')]),
                        (convertT1, reorientT1, [('out_file', 'in_file')]),
                        (reorientT1, makeTemplate, [('out_file', 'subject_T1s')]),
@@ -127,104 +131,4 @@ template_flow.connect([(fs_source, convertT1, [('T1','in_file')]),
 template_flow.base_dir = workflow_dir
 template_flow.write_graph(graph2use = 'flat')
 template_flow.run()
-
-
-# In[ ]:
-
-
-######### Tissue creation nodes: segment template subjects to 5 tissue classes #########
-### 1)CSF, 2)cortical gray matter, 3)white matter, 4)subcortical gray matter, and 5)whole brain
-
-#convert freesurfer brainmask files to .nii
-convert_to_nii = MapNode(MRIConvert(out_file='brainmask.nii.gz',
-                                    out_type='niigz'), 
-                         name='convert_to_nii', 
-                         iterfield = ['in_file'])
-
-#reorient files for safety :)
-reorient_to_std = MapNode(Reorient2Std(),
-                         name = 'reorient_to_std',
-                         iterfield = ['in_file'])
-
-
-#brainmask gets run through segmentation (2) ---> results in segmentation into 3 tissue classes (wm, gm, csf)
-segment = MapNode(FAST(number_classes = 3, 
-                       segments=True, 
-                       no_bias=True), 
-                  name = 'segment', 
-                  iterfield = ['in_files'])
-
-# Convert freesurfer aseg to nii
-convert_aseg = MapNode(MRIConvert(out_file='aseg.nii',
-                                    out_type='niigz'), 
-                         name='convert_aseg', 
-                         iterfield = ['in_file'])
-
-# Reorient aseg to standard
-reorient_aseg = MapNode(Reorient2Std(),
-                         name = 'reorient_aseg',
-                         iterfield = ['in_file'])
-
-# Create subcortical and cortical gray matter masks <-- custom function
-
-
-# make brainmask based on tissue segmentation (sc gm + cortical gm + wm) <-- custom function
-
-
-# In[ ]:
-
-
-######### Tissue segmentation workflow #########
-segment_flow = Workflow(name = "segment_flow")
-segment_flow.connect([(fs_source, convert_to_nii, [('brainmask','in_file')]),
-                      (convert_to_nii, reorient_to_std, [('out_file', 'in_file')]),
-                      (reorient_to_std, segment, [('out_file', 'in_files')]),
-                      (segment, convert_aseg, [('tissue_class_files', 'in_file')]),
-                      (convert_aseg, reorient_aseg, [('out_file', 'in_file')]),
-                      (reorient_aseg, datasink, [('out_file', 'tissue_class_files')]),
-                      #(segment, datasink, [('tissue_class_files', 'tissue_class_files')])
-                     ])
-
-segment_flow.base_dir = workflow_dir
-segment_flow.write_graph(graph2use = 'flat')
-segment_flow.run('MultiProc', plugin_args={'n_procs': 1})
-
-
-# In[ ]:
-
-
-######### Tissue priors Nodes #########
-# brainmask gets registered to template (1) ---> ---> FLIRT (fsl)-results in registration matrix and registered brainmask.nii
-
-
-#apply transformation from registered brainmask to tissue segmentations ---> FLIRT (fsl)
-
-
-#average each of the tissue classes together
-
-
-# In[ ]:
-
-
-######### Tissue priors workflow #########
-
-
-# In[ ]:
-
-
-######### Cortical thickness functions #########
-
-cmd = 'antsCorticalThickness.sh -d 3 -a t1.nii.gz -e Template.nii.gz -m brainmask.nii.gz -p segmentationPriors%d.nii.gz -o subject'
-
-
-# In[ ]:
-
-
-######### Cortical thickness nodes #########
-
-
-# In[ ]:
-
-
-######### Cortical thickness workflow #########
 
